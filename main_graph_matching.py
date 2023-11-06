04598c60ad7350af2c98d71849884deb90622cd7
Tanmoy <mukherjee.tanmoy@gmail.com>
2023-11-06 01:41:12 +0100

    Add files via upload

---
+++ main_graph_matching.py
@@ -0,0 +1,98 @@
+import os
+import time
+import copy
+import argparse
+import numpy as np
+import torch
+import torch.nn as nn
+from torchvision.utils import save_image
+from utils import get_loops, get_dataset, get_network, get_eval_pool, evaluate_synset, get_daparam, match_loss, get_time, TensorDataset, epoch, DiffAugment, ParamDiffAug
+
+
+parser = argparse.ArgumentParser(description='Parameter Processing')
+parser.add_argument('--dataset', type=str, default='CIFAR10', help='dataset')
+parser.add_argument('--model', type=str, default='ConvNet', help='model')
+parser.add_argument('--ipc', type=int, default=50, help='image(s) per class')
+parser.add_argument('--eval_mode', type=str, default='SS', help='eval_mode') # S: the same to training model, M: multi architectures,  W: net width, D: net depth, A: activation function, P: pooling layer, N: normalization layer,
+parser.add_argument('--num_exp', type=int, default=5, help='the number of experiments')
+parser.add_argument('--num_eval', type=int, default=20, help='the number of evaluating randomly initialized models')
+parser.add_argument('--epoch_eval_train', type=int, default=1000, help='epochs to train a model with synthetic data') # it can be small for speeding up with little performance drop
+parser.add_argument('--Iteration', type=int, default=20000, help='training iterations')
+parser.add_argument('--lr_img', type=float, default=1.0, help='learning rate for updating synthetic images')
+parser.add_argument('--lr_net', type=float, default=0.01, help='learning rate for updating network parameters')
+parser.add_argument('--batch_real', type=int, default=256, help='batch size for real data')
+parser.add_argument('--batch_train', type=int, default=256, help='batch size for training networks')
+parser.add_argument('--init', type=str, default='real', help='noise/real: initialize synthetic images from random noise or randomly sampled real images.')
+parser.add_argument('--dsa_strategy', type=str, default='color_crop_cutout_flip_scale_rotate', help='differentiable Siamese augmentation strategy')
+parser.add_argument('--data_path', type=str, default='data', help='dataset path')
+parser.add_argument('--save_path', type=str, default='result', help='path to save results')
+parser.add_argument('--dis_metric', type=str, default='ours', help='distance metric')
+
+args = parser.parse_args()
+args.method = 'DM'
+args.outer_loop, args.inner_loop = get_loops(args.ipc)
+args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+args.dsa_param = ParamDiffAug()
+args.dsa = False if args.dsa_strategy in ['none', 'None'] else True
+
+if not os.path.exists(args.data_path):
+   os.mkdir(args.data_path)
+
+if not os.path.exists(args.save_path):
+   os.mkdir(args.save_path)
+
+eval_it_pool = np.arange(0, args.Iteration+1, 2000).tolist() if args.eval_mode == 'S' or args.eval_mode == 'SS' else [args.Iteration] # The list of iterations when we evaluate models and record results.
+print('eval_it_pool: ', eval_it_pool)
+channel, im_size, num_classes, class_names, mean, std, dst_train, dst_test, testloader = get_dataset(args.dataset, args.data_path)
+model_eval_pool = get_eval_pool(args.eval_mode, args.model, args.model)
+
+
+accs_all_exps = dict() # record performances of all experiments
+for key in model_eval_pool:
+    accs_all_exps[key] = []
+
+data_save = []
+
+
+for exp in range(args.num_exp):
+    print('\n================== Exp %d ==================\n '%exp)
+    print('Hyper-parameters: \n', args.__dict__)
+    print('Evaluation model pool: ', model_eval_pool)
+
+    ''' organize the real dataset '''
+    images_all = []
+    labels_all = []
+    indices_class = [[] for c in range(num_classes)]
+
+    images_all = [torch.unsqueeze(dst_train[i][0], dim=0) for i in range(len(dst_train))]
+    labels_all = [dst_train[i][1] for i in range(len(dst_train))]
+    for i, lab in enumerate(labels_all):
+        indices_class[lab].append(i)
+    images_all = torch.cat(images_all, dim=0).to(args.device)
+    labels_all = torch.tensor(labels_all, dtype=torch.long, device=args.device)
+
+
+
+    for c in range(num_classes):
+        print('class c = %d: %d real images'%(c, len(indices_class[c])))
+
+    def get_images(c, n): # get random n images from class c
+        idx_shuffle = np.random.permutation(indices_class[c])[:n]
+        return images_all[idx_shuffle]
+
+    for ch in range(channel):
+        print('real images channel %d, mean = %.4f, std = %.4f'%(ch, torch.mean(images_all[:, ch]), torch.std(images_all[:, ch])))
+
+
+    ''' initialize the synthetic data '''
+    image_syn = torch.randn(size=(num_classes*args.ipc, channel, im_size[0], im_size[1]), dtype=torch.float, requires_grad=True, device=args.device)
+    label_syn = torch.tensor([np.ones(args.ipc)*i for i in range(num_classes)], dtype=torch.long, requires_grad=False, device=args.device).view(-1) # [0,0,0, 1,1,1, ..., 9,9,9]
+
+    if args.init == 'real':
+       print('initialize synthetic data from random real images')
+       for c in range(num_classes):
+           image_syn.data[c*args.ipc:(c+1)*args.ipc] = get_images(c, args.ipc).detach().data
+    else:
+        print('initialize synthetic data from random noise')
+
+
